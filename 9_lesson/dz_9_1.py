d2c0face197e1b9ead9d047e382713db45a13b4d
Vadym_Fustii <vadimfustie1995@gmail.com>
2022-11-08 17:52:14 +0200

    First task done

---
+++ 9_lesson/dz_9_1.py
@@ -0,0 +1,14 @@
+class Auto():
+    def __init__(self, model, color, engine_volume):
+        self.model = model
+        self.color = color
+        self.engine_volume = engine_volume
+    @staticmethod
+    def forvard():
+        print("move forvard")
+    @staticmethod
+    def backwards():
+        print("move backvards")
+car1 = Auto("Citroen", "maroon", 1.6)
+car1.forvard()
+car1.backwards()
